f9a9f00be4ff0dd6781874f1752c908911be2df5
KIMTAENYEON <k9313307@naver.com>
2021-10-29 17:28:03 +0900

    3-13 for문 1~5출력

---
+++ tn/src/day3/ForNumberEx1.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class ForNumberEx1 {
+	//for문 1~5출력
+	public static void main(String[] args) {
+		/* 1부터 5까지 출력하는 코드를 for문으로 작성하세요.
+		 * */
+		int i;
+		for(i = 1; i <= 5; i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
