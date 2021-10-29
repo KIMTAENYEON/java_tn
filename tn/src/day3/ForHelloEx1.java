2ab2081e54f5ed54fb02c29f8131f46c6a9c4d80
KIMTAENYEON <k9313307@naver.com>
2021-10-29 16:28:35 +0900

    3-12 for문 Hello

---
+++ tn/src/day3/ForHelloEx1.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class ForHelloEx1 {
+	//for문 Hello
+	public static void main(String[] args) {
+		/* Hello를 5번 출력하는 코드를 작성하세요.
+		 * 규칙성 : Hello를 출력
+		 * 반복획수 : 5번, 숫자는 중요하지 않음.
+		 * */
+		int i;
+		
+		for(i = 1; i <= 5; i++)
+		{
+			System.out.println("Hello");
+		}		
+		
+	}
+
+}
