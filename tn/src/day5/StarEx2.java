8bd5540c3b0013a3f301fd324c144e6aa80cc754
KIMTAENYEON <k9313307@naver.com>
2021-11-02 13:57:41 +0900

    5-10 별 1~5출력

---
+++ tn/src/day5/StarEx2.java
@@ -0,0 +1,44 @@
+package day5;
+
+public class StarEx2 {
+	//별 1~5출력
+	public static void main(String[] args) {
+		/* 다음과 같이 출력되도록 반복문을 이용하여 코드를 작성하세요.
+		 * *
+		 * **
+		 * ***
+		 * ****
+		 * *****
+		 * 반복횟수 : i는 1부터 5까지 1씩 증가
+		 * 규칙성 : *을 i개 출력하고 엔터를 출력
+		 * 		반복횟수 : j를 1부터 i까지 1씩 증가
+		 * 		규칙성 : *를 출력
+		 * 		엔터를 출력
+		 * */
+		//for
+		int i,j;
+		for(i = 1; i <= 5; i++)
+		{
+			for(j = 1; j <= i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		//while
+		i = 1;
+		while(i <= 5)
+		{
+			j = 1;
+			while(j++ <= i)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
