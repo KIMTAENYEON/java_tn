018792a4c26f4e3179b0f7071c0b707bc108d212
KIMTAENYEON <k9313307@naver.com>
2021-11-08 18:11:37 +0900

    9-6 클래스 사용이유

---
+++ tn/src/day9/ClassEx5.java
@@ -0,0 +1,17 @@
+package day9;
+
+public class ClassEx5 {
+	//클래스 사용이유
+	public static void main(String[] args) {
+		/* 학생 1명의 정보를 관리하는 예제 : 클래스 사용 x
+		 * */
+		int size = 5;
+		String sName[] = new String[size], name [] = new String[size];
+		int grade[] = new int[size], classNum[] = new int [size], num[] = new int [size];
+
+		/* 학생 1명의 정보를 관리하는 예제 : 클래스 사용 HighSchoolStudent
+		 * */
+		HighSchoolStudent std[] = new HighSchoolStudent[size];
+	}
+
+}
