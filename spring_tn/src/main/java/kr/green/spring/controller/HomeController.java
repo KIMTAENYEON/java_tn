8cb9881292991fa95a37e74ffe26c686fa832c8c
KIMTAENYEON <k9313307@naver.com>
2022-01-06 17:37:44 +0900

    스프링 로그아웃 기능 및 게시글 등록 기능 구현 완료

--- spring_tn/src/main/java/kr/green/spring/controller/HomeController.java
+++ spring_tn/src/main/java/kr/green/spring/controller/HomeController.java
@@ -1,5 +1,7 @@
 package kr.green.spring.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,15 +40,19 @@ public class HomeController {
 	public ModelAndView loginPost(ModelAndView mv, MemberVO member) {	
 		System.out.println("/login:post : " + member);
 		MemberVO user = memberService.login(member);
-		System.out.println(user);
-		mv.setViewName("/member/login");
+		if(user == null) {
+			mv.setViewName("redirect:/login");
+		}else {
+			mv.addObject("user", user);
+			mv.setViewName("redirect:/");
+		}
+		
 		return mv;
 	}
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public ModelAndView signupGet(ModelAndView mv, MemberVO user) {	
 		System.out.println("/signup:get : ");
 		mv.setViewName("/member/signup");
-		
 		return mv;
 	}
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
@@ -57,6 +63,13 @@ public class HomeController {
 		}else{
 			mv.setViewName("redirect:/signup");	
 		};	
+		return mv;
+	}
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public ModelAndView logoutGet(ModelAndView mv, HttpServletRequest requst) {	
+		System.out.println("/logout:get : ");
+		requst.getSession().removeAttribute("user");
+		mv.setViewName("redirect:/");	
 		return mv;
 	}
 }
