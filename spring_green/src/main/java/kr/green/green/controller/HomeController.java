94782f27ec76395d1d32f98f572fdb8f727425f9
KIMTAENYEON <k9313307@naver.com>
2022-01-10 17:29:49 +0900

    스프링 패키지 생성 및 DB연동 (복습)

--- spring_green/src/main/java/kr/green/green/controller/HomeController.java
+++ spring_green/src/main/java/kr/green/green/controller/HomeController.java
@@ -1,12 +1,18 @@
 package kr.green.green.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.green.green.service.MemberService;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	MemberService memberService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView homeGet(ModelAndView mv) {
---
+++ spring_green/src/main/java/kr/green/green/dao/MemberDAO.java
@@ -0,0 +1,5 @@
+package kr.green.green.dao;
+
+public interface MemberDAO {
+
+}
