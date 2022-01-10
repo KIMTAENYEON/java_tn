7cad8929fe277875bb34aa25e2638e6438e60751
KIMTAENYEON <k9313307@naver.com>
2022-01-10 13:06:54 +0900

    스프링 게시글 수정 기능 완료

--- spring_tn/src/main/java/kr/green/spring/controller/BoardController.java
+++ spring_tn/src/main/java/kr/green/spring/controller/BoardController.java
@@ -47,7 +47,6 @@ public class BoardController {
 	}
 	@RequestMapping(value = "/detail")
 	public ModelAndView boardDetail(ModelAndView mv, Integer bd_num) {
-		mv.setViewName("/board/detail");
 		//게시글 번호 확인
 		//System.out.println("게시글 번호 : " + bd_num);
 		//게시글 = boardService.게시글가져오기(게시글번호);
@@ -56,6 +55,7 @@ public class BoardController {
 		//System.out.println(board);
 		//화면에게 게시글을 전달 
 		mv.addObject("board", board);
+		mv.setViewName("/board/detail");
 		return mv;
 	}
 	@RequestMapping(value = "/delete", method = RequestMethod.GET)
@@ -95,6 +95,18 @@ public class BoardController {
 		//서비스가 보내준 게시글의 작성자와 로그인한 회원 아이디가 일치하는지 확인
 		//컨트롤러가 서비스가 보내준 게시글 정보를 가지고 추가 확인 
 		mv.setViewName("/board/modify");
+		return mv;
+	}
+	@RequestMapping(value = "/modify", method=RequestMethod.POST)
+	public ModelAndView boardModifyPost(ModelAndView mv, BoardVO board) { 
+		//화면에서 수정한 게시글 정보가 넘어오는지 확인
+		//System.out.println("게시글 : " + board);
+		//서비스에게 게시글 정보를 주면서 업데이트 하라고 시킴
+		//서비스.게시글업데이트(게시글정보)
+		boardService.updateBoard(board);
+		//게시글 번호를 넘겨줌
+		mv.addObject("bd_num", board.getBd_num());
+		mv.setViewName("redirect:/board/detail");
 		return mv;
 	}
 }
