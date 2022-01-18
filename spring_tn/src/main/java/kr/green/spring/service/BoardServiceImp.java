a4e00a9b0f5c6dd84447fbbf86ea32c06646c6ef
KIMTAENYEON <k9313307@naver.com>
2022-01-18 17:19:36 +0900

    스프링 공지사항 적용(모든 회원)

--- spring_tn/src/main/java/kr/green/spring/service/BoardServiceImp.java
+++ spring_tn/src/main/java/kr/green/spring/service/BoardServiceImp.java
@@ -36,8 +36,8 @@ public class BoardServiceImp implements BoardService{
 	}
 
 	@Override
-	public List<BoardVO> getBoardList(String type, Criteria cri) {
-		return boardDao.getBoardList(type, cri);
+	public List<BoardVO> getBoardList(Criteria cri) {
+		return boardDao.getBoardList(cri);
 	}
 
 	@Override
@@ -162,7 +162,7 @@ public class BoardServiceImp implements BoardService{
 	}
 
 	@Override
-	public int getTotalCount(String type, Criteria cri) {
-		return boardDao.selectCountBoard(type, cri);
+	public int getTotalCount(Criteria cri) {
+		return boardDao.selectCountBoard(cri);
 	}
 }
